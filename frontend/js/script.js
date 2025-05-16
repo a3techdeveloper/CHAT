60f68857c53afbe1cf42abba7c638d41feb9f966
a3techdeveloper <a3tech.ead@gmail.com>
2025-05-16 20:50:59 -0300

    Alterando servidor

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -81,7 +81,7 @@ const handleLogin = (event) => {
     login.style.display = "none"
     chat.style.display = "flex"
  
-    websocket = new WebSocket("ws://chat-lje3.onrender.com")
+    websocket = new WebSocket("wss://chat-lje3.onrender.com")
     websocket.onmessage = processMessage
 }
 
